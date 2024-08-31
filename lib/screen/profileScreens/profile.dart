24484a444151b539669f48804313550c87b091d1
Faishana Rafeesh <151698507+Faishana@users.noreply.github.com>
2024-08-31 11:31:13 +0530

    Modified profile screen

--- lib/screen/profileScreens/profile.dart
+++ lib/screen/profileScreens/profile.dart
@@ -5,6 +5,48 @@ class profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon:  const Icon(Icons.arrow_back_ios,
+              color: Colors.blue,),
+              onPressed:  (){
+                
+              }),
+        elevation: 1,
+        backgroundColor: Colors.transparent,
+
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 20),
+        child: ListView(
+          children: [
+            const Center(
+              child: Text("Edit Your Profile",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 4, 60, 106),
+                fontStyle: FontStyle.italic,
+              ),),
+            ),
+            SizedBox(height: 15,),
+            Container(
+              
+              width: 150,
+              height: 150,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage("images/doctor1.jpg"),
+                fit: BoxFit.contain,
+                
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+
+    );
   }
 }
