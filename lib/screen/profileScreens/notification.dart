3534c048b45e476da7e0798a35fd03715292b345
Faishana Rafeesh <151698507+Faishana@users.noreply.github.com>
2024-09-01 00:26:53 +0530

    created notification

--- lib/screen/profileScreens/notification.dart
+++ lib/screen/profileScreens/notification.dart
@@ -38,17 +38,92 @@ class ListVeiw extends StatelessWidget {
 
   Widget ListVeiwItem(int index){
     return Container(
-      margin: EdgeInsets.only(left: 10),
-      child: Column(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          message(index)
+          prefixIcon(),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  message(index),
+                  dateTime(index),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
   Widget message(int index){
-    return Container();
+    double textSize = 14;
+    return Container(
+      child: RichText(
+        maxLines: 3,
+        overflow: TextOverflow.ellipsis,
+        text: const TextSpan(
+          text: "Message ",
+          style: TextStyle(
+            color: Colors.black,
+            
+            fontWeight: FontWeight.bold,
+          ),
+          children: 
+          [
+            TextSpan(
+              text: 'Notification Discription',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+          ),
+            ),
+          ],
+        ),
+      ), 
+    );
+  }
+
+  Widget prefixIcon(){
+    return Container(
+      height: 50,
+      width: 50,
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+
+      ),
+      child: const Icon(Icons.notifications,
+      size: 30,
+      color: Colors.grey,),
+    );
+  }
+
+  Widget dateTime(int index){
+    return Container(
+      margin: const EdgeInsets.only(top: 7),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '24/08/2024',
+            style: TextStyle(
+              color:  Colors.black,
+              fontSize: 10,
+            ),
+          ),
+          Text(
+            '06.24PM',
+            style: TextStyle(
+              color:  Colors.black,
+              fontSize: 10,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
